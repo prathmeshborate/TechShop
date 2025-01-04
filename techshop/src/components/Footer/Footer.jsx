a1f226e3762826e7fc9a3a2cfea3065358182ef3
Prathmesh Dilip Borate <prathmeshborate01@gmail.com>
2025-01-04 15:11:07 +0000

    techshop

---
+++ techshop/src/components/Footer/Footer.jsx
@@ -0,0 +1,129 @@
+import React from "react";
+import Logo from '../../assets/logo.png';
+import { FaMobileAlt } from "react-icons/fa";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaLocationArrow,
+} from "react-icons/fa6";
+
+const FooterLinks = [
+  {
+    title: "Home",
+    link: "/#",
+  },
+  {
+    title: "About",
+    link: "/#about",
+  },
+  {
+    title: "Contact",
+    link: "/#contact",
+  },
+  {
+    title: "Blog",
+    link: "/#blog",
+  },
+];
+
+const Footer = () => {
+  return (
+    <div className="dark:bg-gray-950">
+      <div className="container">
+        <div className="grid md:grid-cols-3 pb-20 pt-5">
+          {/* company details */}
+          <div className="py-8 px-4">
+            <a
+              href="#"
+              className="text-primary flex gap-2 font-semibold tracking-widest text-2xl uppercase sm:text-3xl
+"
+            >
+              <img src={Logo} alt="Logo" className='w-10 uppercase'/>
+              techshop
+            </a>
+            <p className="text-gray-500 mt-4">
+              Made with 💖 by Prathmesh Borate
+            </p>
+            <a
+              href="https://prathmeshborate.github.io/Portfolio-Website/"
+              target="_blank"
+              className="inline-block bg-primary/90 text-white py-2 px-4 mt-4 text-sm rounded-full"
+            >
+              Visit My Portfolio
+            </a>
+          </div>
+
+          {/* Footer links */}
+          <div className="col-span-2 grid grid-cols-2 sm:grid-cols-3 md:pl-10">
+            <div className="py-8 px-4">
+              <h1 className="text-xl font-bold sm:text-left mb-3">
+                Important Links
+              </h1>
+              <ul className="space-y-3">
+                {FooterLinks.map((data, index) => (
+                  <li key={index}>
+                    <a
+                      href={data.link}
+                      className="text-gray-600 dark:text-gray-400 hover:dark:text-white hover:text-black duration-300"
+                    >
+                      {data.title}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            {/* second col links */}
+            <div className="py-8 px-4">
+              <h1 className="text-xl font-bold sm:text-left mb-3">
+                Quick Links
+              </h1>
+              <ul className="space-y-3">
+                {FooterLinks.map((data, index) => (
+                  <li key={index}>
+                    <a
+                      href={data.link}
+                      className="text-gray-600 dark:text-gray-400 hover:dark:text-white hover:text-black duration-300"
+                    >
+                      {data.title}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            {/* Company Address */}
+            <div className="py-8 px-4 col-span-2 sm:col-auto">
+              <h1 className="text-xl font-bold sm:text-left mb-3">Address</h1>
+              <div>
+                <div className="flex items-center gap-3">
+                  <FaLocationArrow />
+                  <p>Mumbai, Maharashtra</p>
+                </div>
+                <div className="flex items-center gap-3 mt-6">
+                  <FaMobileAlt />
+                  <p>+91 81086 70904</p>
+                </div>
+
+                {/* social links */}
+                <div className="flex items-center gap-3 mt-6">
+                  <a href="https://www.instagram.com/borate_prathmesh/">
+                    <FaInstagram className="text-3xl hover:text-primary duration-300" />
+                  </a>
+                  <a href="#">
+                    <FaFacebook className="text-3xl hover:text-primary duration-200" />
+                  </a>
+                  <a href="https://www.linkedin.com/in/prathmesh-borate-063b19204/">
+                    <FaLinkedin className="text-3xl hover:text-primary duration-200" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
